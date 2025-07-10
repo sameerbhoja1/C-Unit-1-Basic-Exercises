6efef17503a1327978c78adb401ebd9af6564f5c
SAMEER SORTHIYA <sameerbhoja1@gmail.com>
2025-07-10 15:03:57 +0530

    Create Swap Two Numbers.cpp

---
+++ Swap Two Numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Y, T;
+    cout << "Enter The Value Of A: ";
+    cin >> X;
+    cout << "Enter The Value Of B: ";
+    cin >> Y;
+    temp = X;
+    X = Y;
+    Y = T;
+    cout << "After swap, A = " << X << ", B = " << Y << endl;
+    return 0;
+}
